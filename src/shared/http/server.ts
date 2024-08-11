bfe48f4008d85902c3fe9e5ebdfd698a86773a63
Wayre Avelar <wayre1@gmail.com>
2024-08-11 15:56:31 -0300

    chore: configuracao express, estrutura inicial projeto, etc

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,9 +1,9 @@
-import Express from "express";
-import routes from "./routes";
+import express from "express";
 import cors from "cors";
+import routes from "@shared/routes";
 
-const app = Express();
-app.use(Express.json());
+const app = express();
+app.use(express.json());
 app.use(cors());
 app.use(routes);
 
